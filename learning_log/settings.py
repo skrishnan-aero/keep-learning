8e3d36d841bdd423ccdeb7fa782c188cb63a30e0
skrishnan-aero <skrishnan.aero@gmail.com>
2018-09-20 18:44:13 +0530

    corrected static file directory_try4

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -147,7 +147,7 @@ if os.getcwd() == '/app':
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
